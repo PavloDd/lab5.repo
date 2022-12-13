fe8fc4b5fbf0750e2c1a70bf2fb5946ef3d18896
PavloDd <86347147+PavloDd@users.noreply.github.com>
2022-12-13 10:52:09 +0200

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -19,21 +19,21 @@ void File::show_File() {
 	std::cout << "name=" << this->name << "\t extentoin=" << this->extention << "\t size=" << this->size << std::endl;
 };
 
-void File:: add_file(File& a) {
+void File:: add_File(File& a) {
 	Folder.push_back(a);
 };
 
-void File::show_folder() {
+void File::show_Folder() {
 	for (int j = 0; j < Folder.size(); j++) {
 		std::cout << "name=" << Folder[j].name << "\t extentoin=" << Folder[j].extention << "\t size=" << Folder[j].size << std::endl;
 	}
 };
-void File::output() {
+void File::output_Folder() {
 	std::cout << "Folder`s name:" << this->name << std::endl;
 	for (int i = 0; i < Folder.size(); i++) {
 		if (Folder[i].type== "folder") {
 			std::cout << "name:" << Folder[i].name << std::endl;
-			Folder[i].show_folder();
+			Folder[i].show_Folder();
 		}
 		else {
 			Folder[i].show_File();
