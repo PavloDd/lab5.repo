b49de2b4cde6737a25ca586d514a0171600b0c06
PavloDd <86347147+PavloDd@users.noreply.github.com>
2022-12-20 18:56:06 +0200

    i changed my file.cpp

--- file.cpp
+++ file.cpp
@@ -8,35 +8,35 @@ File::File() {
 	type = "folder";
 };
 
-File::File(std::string aName, std::string aExtention, std::string aSize, std::string aType) {
-	this->name = aName;
-	this->extention = aExtention;
-	this->size = aSize;
-	this->type = aType;
+File::File(string Name, string Extention, string Size, string Type) {
+	this->name = Name;
+	this->extention = Extention;
+	this->size = Size;
+	this->type = Type;
 }; 
 
-void File::show_File() {
+void File::show_file() {
 	std::cout << "name=" << this->name << "\t extentoin=" << this->extention << "\t size=" << this->size << std::endl;
 };
 
-void File:: add_File(File& a) {
+void File:: add_file(File& a) {
 	Folder.push_back(a);
 };
 
-void File::show_Folder() {
+void File::show_folder() {
 	for (int j = 0; j < Folder.size(); j++) {
 		std::cout << "name=" << Folder[j].name << "\t extentoin=" << Folder[j].extention << "\t size=" << Folder[j].size << std::endl;
 	}
 };
-void File::output_Folder() {
+void File::output_folder() {
 	std::cout << "Folder`s name:" << this->name << std::endl;
 	for (int i = 0; i < Folder.size(); i++) {
 		if (Folder[i].type== "folder") {
 			std::cout << "name:" << Folder[i].name << std::endl;
-			Folder[i].show_Folder();
+			Folder[i].show_folder();
 		}
 		else {
-			Folder[i].show_File();
+			Folder[i].show_file();
 		}
 	};
 };
